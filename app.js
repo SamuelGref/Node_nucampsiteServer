69082afefb5c09051c33b79e39b96df6b4212857
Samuel Gref <samgref@gmail.com>
2021-05-25 11:22:15 -0400

    Basic Auth

--- app.js
+++ app.js
@@ -36,7 +36,6 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
 
 // Authentication
 
@@ -68,7 +67,7 @@ function auth(req, res, next) {
 app.use(auth);
 
 // view engine setup
-
+app.use(express.static(path.join(__dirname, "public")));
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/campsites", campsiteRouter);
