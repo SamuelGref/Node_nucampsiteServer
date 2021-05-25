5b14d8767f55de7317ce5e1e0c4e138063b00a66
Samuel Gref <samgref@gmail.com>
2021-05-25 17:34:47 -0400

    Express Session Part 2

--- app.js
+++ app.js
@@ -51,34 +51,18 @@ app.use(
 
 // Authentication
 
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+
 function auth(req, res, next) {
   console.log(req.session);
 
   if (!req.session.user) {
-    const authHeader = req.headers.authorization;
-    if (!authHeader) {
-      const err = new Error("You are not authenticated!");
-      res.setHeader("WWW-Authenticate", "Basic");
-      err.status = 401;
-      return next(err);
-    }
-
-    const auth = Buffer.from(authHeader.split(" ")[1], "base64")
-      .toString()
-      .split(":");
-    const user = auth[0];
-    const pass = auth[1];
-    if (user === "admin" && pass === "password") {
-      req.session.user = "admin";
-      return next(); // authorized
-    } else {
-      const err = new Error("You are not authenticated!");
-      res.setHeader("WWW-Authenticate", "Basic");
-      err.status = 401;
-      return next(err);
-    }
+    const err = new Error("You are not authenticated!");
+    err.status = 401;
+    return next(err);
   } else {
-    if (req.session.user === "admin") {
+    if (req.session.user === "authenticated") {
       return next();
     } else {
       const err = new Error("You are not authenticated!");
@@ -92,8 +76,7 @@ app.use(auth);
 
 // view engine setup
 app.use(express.static(path.join(__dirname, "public")));
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
+
 app.use("/campsites", campsiteRouter);
 app.use("/promotions", promotionRouter);
 app.use("/partners", partnerRouter);
