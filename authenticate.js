c002addcead74bece2eba518b6efca104fbbc95a
Samuel Gref <samgref@gmail.com>
2021-06-02 22:22:33 -0400

    OAuth

--- authenticate.js
+++ authenticate.js
@@ -5,6 +5,7 @@ const User = require("./models/user");
 const JwtStrategy = require("passport-jwt").Strategy;
 const ExtractJwt = require("passport-jwt").ExtractJwt;
 const jwt = require("jsonwebtoken"); // used to create, sign, and verify tokens
+const FacebookTokenStrategy = require("passport-facebook-token");
 
 const config = require("./config.js");
 
@@ -35,6 +36,8 @@ exports.jwtPassport = passport.use(
   })
 );
 
+exports.verifyUser = passport.authenticate("jwt", { session: false });
+
 exports.verifyAdmin = (req, res, next) => {
   if (req.user.admin) {
     res.statusCode = 200;
@@ -46,4 +49,33 @@ exports.verifyAdmin = (req, res, next) => {
   }
 };
 
-exports.verifyUser = passport.authenticate("jwt", { session: false });
+exports.facebookPassport = passport.use(
+  new FacebookTokenStrategy(
+    {
+      clientID: config.facebook.clientId,
+      clientSecret: config.facebook.clientSecret,
+    },
+    (accessToken, refreshToken, profile, done) => {
+      User.findOne({ facebookId: profile.id }, (err, user) => {
+        if (err) {
+          return done(err, false);
+        }
+        if (!err && user) {
+          return done(null, user);
+        } else {
+          user = new User({ username: profile.displayName });
+          user.facebookId = profile.id;
+          user.firstname = profile.name.givenName;
+          user.lastname = profile.name.familyName;
+          user.save((err, user) => {
+            if (err) {
+              return done(err, false);
+            } else {
+              return done(null, user);
+            }
+          });
+        }
+      });
+    }
+  )
+);
--- config.js
+++ config.js
@@ -2,3 +2,12 @@ module.exports = {
   secretKey: "12345-67890-09876-54321",
   mongoUrl: "mongodb://localhost:27017/nucampsite",
 };
+
+module.exports = {
+  secretKey: "12345-67890-09876-54321",
+  mongoUrl: "mongodb://localhost:27017/nucampsite",
+  facebook: {
+    clientId: "YOUR FACEBOOK APP ID HERE",
+    clientSecret: "YOUR FACEBOOK APP SECRET HERE",
+  },
+};
